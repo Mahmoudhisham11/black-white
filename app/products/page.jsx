42840c03e772ac08b0ccb731a25b991db2248b90
Mahmoud Hisham <mahmoudhisham11@gmail.com>
2025-12-19 09:04:21 +0200

    Add new files and updates

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -678,7 +678,7 @@ function Products() {
             <svg id="barcode" class="barcode"></svg>
             <div class="container">
               <div class="price">${product.code ?? ""} </div>
-              <div class="price">${product.buyPrice ?? ""} EGP </div>
+              <div class="price">${product.sellPrice ?? ""} EGP </div>
             </div>
           </div>
           <script>
